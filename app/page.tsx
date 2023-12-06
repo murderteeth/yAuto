51e6ecd77d7ae92fd881f774789cefabbc3bdb82
murderteeth <murdertxxth@gmail.com>
2023-12-06 04:13:22 +0000

    Add header

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,17 @@
 
 import Image from 'next/image'
 import { WithYearn } from '@yearn-finance/web-lib/contexts/WithYearn'
-import { localhost, mainnet } from '@wagmi/chains'
+import { localhost, mainnet, polygon } from '@wagmi/chains'
+import Header from '@/components/header'
 
 export default function Home() {
-  return <WithYearn supportedChains={[mainnet, localhost]}>
-    <main className="flex min-h-screen flex-col items-center justify-center gap-4 p-24">
-      <Image priority={true} src="/otto.png" alt="yAuto" width={200} height={200} />
-      <h1 className="font-[900] text-6xl">yAuto</h1>
-    </main>
+  return <WithYearn supportedChains={[mainnet, polygon, localhost]}>
+    <>
+      <Header />
+      <main className="flex min-h-screen flex-col items-center justify-center gap-4 p-24">
+        <Image priority={true} src="/otto.png" alt="yAuto" width={200} height={200} />
+        <h1 className="font-[900] text-6xl">yAuto</h1>
+      </main>
+    </>
   </WithYearn>
 }
